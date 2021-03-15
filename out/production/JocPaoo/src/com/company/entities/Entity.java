6021acc08f01826da783fa6695b2284ba406ec12
robert.tutuianu <robertzel_2001@yahoo.com>
2021-03-15 11:33:10 +0200

    Gata tut15

--- out/production/JocPaoo/src/com/company/entities/Entity.java
+++ out/production/JocPaoo/src/com/company/entities/Entity.java
@@ -5,13 +5,48 @@ import java.awt.*;
 public abstract class Entity {
 
     protected float x,y;
+    protected int width, height;
 
-    public Entity(float x , float y){
+    public Entity(float x , float y,int width , int height){
         this.x = x;
         this.y = y;
+        this.width = width;
+        this.height = height;
     }
 
     public abstract void tick();
     public abstract void render(Graphics g);
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
 
 }
--- src/com/company/entities/Player.java
+++ src/com/company/entities/Player.java
@@ -9,24 +9,32 @@ public class Player extends Creature {
 
     private Game game;
     public Player(Game game, float x, float y){
-        super(x,y);
+        super(x,y,DEFAULT_CREATURE_WIDTH,DEFAUL_CREATURE_HEIGHT);
         this.game = game;
     }
 
     @Override
     public void tick() {
+        getInput();
+        move();
+    }
+
+    private void getInput(){
+        xMove = 0;
+        yMove = 0;
+
         if(game.getKeyManager().up)
-            y -= 3;
+            yMove = -speed;
         if(game.getKeyManager().down)
-            y += 3;
+            yMove = speed;
         if(game.getKeyManager().left)
-            x -= 3;
+            xMove = -speed;
         if(game.getKeyManager().right)
-            x += 3;
+            xMove = speed;
     }
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Assets.player,(int)x,(int)y,null);
+        g.drawImage(Assets.player,(int)x,(int)y,width,height,null);
     }
 }
