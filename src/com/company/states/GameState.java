35ce4793a0551dfb047554d3e856f52790a90369
Robert Tutuianu <robertzzel_2001@yahoo.com>
2021-05-13 10:43:42 +0300

    Timer class

--- src/com/company/states/GameState.java
+++ src/com/company/states/GameState.java
@@ -3,6 +3,7 @@ package com.company.states;
 import com.company.Handler;
 import com.company.UI.UIManager;
 import com.company.entities.Player;
+import com.company.utils.Timer;
 import com.company.worlds.World;
 
 import java.awt.*;
@@ -12,43 +13,19 @@ public class GameState extends State {
     public static int lvl = 1;
     private Player player;
     private World world;
-    private int min=3,sec=0;
-    private long currentTime=System.currentTimeMillis();
+    private Timer timer;
 
     public GameState(Handler handler){
         super(handler);
         world = new World(handler,"res/worlds/world1.txt");
         handler.setWorld(world);
-
-    }
-
-    public void clock(){
-        if(System.currentTimeMillis() - currentTime > 1000){
-            if(sec+min>0){
-                if(sec == 0 && min > 0){
-                    min--;
-                    sec=59;
-                }else{
-                    sec--;
-                }
-                handler.getGame().getDispaly().mesaj.setText(min+":"+sec);
-                System.out.println("schimbare");
-            }else{
-                this.min=2;
-                this.sec=59;
-                State.setState(handler.getGame().menuState);
-                handler.getMouseManager().setUiManager(handler.getGame().uiManagerMenu);
-
-            }
-            currentTime = System.currentTimeMillis();
-        }
-
+        this.timer = new Timer(handler,1,0);
     }
 
     @Override
     public void tick() {
+        timer.tick();
         world.tick();
-        clock();
         verifEscapeKey();
     }
 
---
+++ src/com/company/utils/Timer.java
@@ -0,0 +1,59 @@
+package com.company.utils;
+
+import com.company.Handler;
+import com.company.states.State;
+
+public class Timer {
+
+    private int min,sec;
+    private int backupMin,backupSec;
+    private Handler handler;
+    private long currentTime;
+
+    public Timer(Handler handler, int min, int sec){
+        this.min = min;
+        this.sec = sec;
+        this.backupMin = min;
+        this.backupSec = sec;
+        this.currentTime = System.currentTimeMillis();
+        this.handler = handler;
+    }
+
+    public void tick(){
+        if(System.currentTimeMillis() - currentTime > 1000){
+            if(sec+min>0){
+                if(sec == 0 && min > 0){
+                    min--;
+                    sec=59;
+                }else{
+                    sec--;
+                }
+
+                //actiune la fiecare secunda
+                actionOnSecond(min+":"+sec);
+
+            }else{
+                finalClock();
+            }
+            currentTime = System.currentTimeMillis();
+        }
+    }
+
+    private void finalClock(){
+        resetClock();
+        State.setState(handler.getGame().menuState);
+        handler.getMouseManager().setUiManager(handler.getGame().uiManagerMenu);
+    }
+
+    private void resetClock(){
+        this.min = this.backupMin;
+        this.sec = this.backupSec;
+        this.currentTime = System.currentTimeMillis();
+    }
+
+    private void actionOnSecond(String timp){
+        handler.getGame().getDispaly().mesaj.setText(timp);
+        System.out.println("schimbare");
+    }
+
+}
